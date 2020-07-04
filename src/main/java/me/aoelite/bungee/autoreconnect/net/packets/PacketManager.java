63ee506aa024b5e0c636303802f5a14383f3693d
PseudoResonance <kaio11602@gmail.com>
2020-07-04 15:01:04 -0700

    Update 1.5
    Fixed loading plugin without Protocolize

--- src/main/java/me/aoelite/bungee/autoreconnect/net/packets/PacketManager.java
+++ src/main/java/me/aoelite/bungee/autoreconnect/net/packets/PacketManager.java
@@ -2,11 +2,16 @@ package me.aoelite.bungee.autoreconnect.net.packets;
 
 import de.exceptionflug.protocolize.api.protocol.ProtocolAPI;
 import me.aoelite.bungee.autoreconnect.AutoReconnect;
+import net.md_5.bungee.protocol.DefinedPacket;
 
 public class PacketManager {
 	
 	public static void register(AutoReconnect instance) {
 		ProtocolAPI.getPacketRegistration().registerPlayClientPacket(PositionLookPacket.class, PositionLookPacket.MAPPING);
+	}
+	
+	public static DefinedPacket getPositionLookPacket() {
+		return new PositionLookPacket(0, 0, 0, 0f, 0f, (byte) 0, 0);
 	}
 
 }
